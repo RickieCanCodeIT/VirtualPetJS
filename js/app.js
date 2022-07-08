708f2e895b300123acea1b16d3aa124729d89eda
RickieCanCodeIT <rickie@WeCanCodeIT.org>
2022-07-08 10:59:40 -0400

    Friday lecture changes

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const furColorEl = document.querySelector("#furColor");
 const soundEl = document.querySelector("#sound");
 const statusEl = document.querySelector("#status");
 
-const jeremy = new VirtualPet("Jeremy", "Brown", "Woof");
+const jeremy = new VirtualPet(virtualPet.name, virtualPet.furColor, virtualPet.sound);
 
 
 petNameEl.innerText = jeremy.name;
@@ -45,3 +45,47 @@ setInterval(() => {
     jeremy.tick();
     statusEl.innerText = jeremy.getStatus();
 }, 1000);
+
+let myFavoriteFruits = ["bananas", "oranges", "papaya", "mango", "pineapple"];
+let myEmptyArray = new Array(5);
+// myEmptyArray[0] = "bananas";
+// myEmptyArray[1] = "oranges";
+// myEmptyArray[2] = "papaya";
+// myEmptyArray[3] = "mango";
+// myEmptyArray[4] = "pineapple";
+console.log(myFavoriteFruits);
+console.log(myEmptyArray);
+console.log(jeremy);
+
+
+let poppedFruit = myFavoriteFruits.pop();
+console.log(poppedFruit);
+console.log(myFavoriteFruits);
+
+myFavoriteFruits.push("apple");
+console.log(myFavoriteFruits);
+
+
+let splicedFruit = myFavoriteFruits.splice(2 /*index to start splicing at*/ , 1 /*nuber of items to splice*/ );
+// myFavoriteFruits.splice(2, 0, splicedFruit[0], splicedFruit[1]);
+console.log(splicedFruit);
+console.log(myFavoriteFruits);
+
+
+myFavoriteFruits.unshift("mango");
+console.log(myFavoriteFruits);
+myFavoriteFruits.shift("oranges");
+console.log(myFavoriteFruits);
+
+let numbers = [1.0, 2, 'three', 4, 'five'];
+console.log(numbers);
+
+
+let person = "Taylour";
+let personFruit = "Star Fruit";
+let nameMap = new Array(0);
+nameMap["Rickie"] = ["apples", "pineapple", "Berries"];
+nameMap["Alex"] = "Avocado";
+nameMap["Chandan"] = "bananas";
+nameMap[person] = personFruit;
+console.log(nameMap);
